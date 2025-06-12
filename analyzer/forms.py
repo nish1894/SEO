b1bb490ac58e1fb2d4fc3a4bf91209427fec1a77
nish1894 <nishantkumar8466@gmail.com>
2025-06-12 18:36:12 +0530

    updated, mostly refined, all functions working +

--- analyzer/forms.py
+++ analyzer/forms.py
@@ -1,22 +1,22 @@
-# analyzer/forms.py
-from django import forms
-import requests
-
-class URLForm(forms.Form):
-    url = forms.URLField(
-        label="Site URL",
-        widget=forms.URLInput(attrs={"placeholder": "https://example.com", "class": "w-full"}),
-    )
-
-    def clean_url(self):
-        value = self.cleaned_data['url']
-        # Quick head request to see if the site exists
-        try:
-            resp = requests.head(value, timeout=5, allow_redirects=True)
-            resp.raise_for_status()
-        except requests.exceptions.RequestException:
-            raise forms.ValidationError(
-                "Cannot reach that URL. Please make sure it starts with http:// or https:// " 
-                "and that the site is up."
-            )
-        return value
+# # analyzer/forms.py
+# from django import forms
+# import requests
+#
+# class URLForm(forms.Form):
+#     url = forms.URLField(
+#         label="Site URL",
+#         widget=forms.URLInput(attrs={"placeholder": "https://example.com", "class": "w-full"}),
+#     )
+#
+#     def clean_url(self):
+#         value = self.cleaned_data['url']
+#         # Quick head request to see if the site exists
+#         try:
+#             resp = requests.head(value, timeout=5, allow_redirects=True)
+#             resp.raise_for_status()
+#         except requests.exceptions.RequestException:
+#             raise forms.ValidationError(
+#                 "Cannot reach that URL. Please make sure it starts with http:// or https:// "
+#                 "and that the site is up."
+#             )
+#         return value
